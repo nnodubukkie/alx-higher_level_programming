6c2d5344c3ca8d4a0f26e1b8a944ee7d3d468c0a
nnodubukkie <nnodubukkie@gmail.com>
2022-12-02 18:51:11 -0800

    2nd python task

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -27,7 +27,7 @@ listint_t *insert_node(listint_t **head, int number)
 	while (node && node->next && node->next->n < number)
 		node = node->next;
 
-	new->next = node->next:
+	new->next = node->next;
 	node->next = new;
 
 	return (new);
