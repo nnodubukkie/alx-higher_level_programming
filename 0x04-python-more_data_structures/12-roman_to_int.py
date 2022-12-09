41de81fae9bc102f9908d4adb3a5a497250c7ab9
nnodubukkie <nnodubukkie@gmail.com>
2022-12-09 11:16:36 -0800

    4th python task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,6 @@ def to_subtract(list_num):
     for n in list_num:
         if max_list > n:
             to_sub += n
-    
     return (max_list - to_sub)
 
 
@@ -16,7 +15,6 @@ def roman_to_int(roman_string):
 
     if not isinstance(roman_string, str):
         return 0
-    
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     list_keys = list(rom_n.keys())
 
@@ -34,7 +32,5 @@ def roman_to_int(roman_string):
                     list_num.append(rom_n.get(ch))
 
                 last_rom = rom_n.get(ch)
-    
     num += to_subtract(list_num)
-    
     return (num)
